e0c69390f7ada581c3e3c1b25b7dced7fc200234
muhammadjunaidkhalid <mjk22071998@gmail.com>
2022-03-08 17:42:59 +0500

    model class added

---
+++ lib/model/notes.dart
@@ -0,0 +1,74 @@
+import 'dart:convert';
+
+class Notes {
+  int id;
+  String title;
+  String body;
+  DateTime creationDate;
+  Notes({
+    required this.id,
+    required this.title,
+    required this.body,
+    required this.creationDate,
+  });
+
+  Notes copyWith({
+    int? id,
+    String? title,
+    String? body,
+    DateTime? creationDate,
+  }) {
+    return Notes(
+      id: id ?? this.id,
+      title: title ?? this.title,
+      body: body ?? this.body,
+      creationDate: creationDate ?? this.creationDate,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'title': title,
+      'body': body,
+      'creationDate': creationDate.millisecondsSinceEpoch,
+    };
+  }
+
+  factory Notes.fromMap(Map<String, dynamic> map) {
+    return Notes(
+      id: map['id']?.toInt() ?? 0,
+      title: map['title'] ?? '',
+      body: map['body'] ?? '',
+      creationDate: DateTime.fromMillisecondsSinceEpoch(map['creationDate']),
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory Notes.fromJson(String source) => Notes.fromMap(json.decode(source));
+
+  @override
+  String toString() {
+    return 'Notes(id: $id, title: $title, body: $body, creationDate: $creationDate)';
+  }
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+  
+    return other is Notes &&
+      other.id == id &&
+      other.title == title &&
+      other.body == body &&
+      other.creationDate == creationDate;
+  }
+
+  @override
+  int get hashCode {
+    return id.hashCode ^
+      title.hashCode ^
+      body.hashCode ^
+      creationDate.hashCode;
+  }
+}
