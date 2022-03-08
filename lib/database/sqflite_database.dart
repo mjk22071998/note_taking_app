b46ff4d6918e736fbe0b70bfc0b94e2e35808aff
muhammadjunaidkhalid <mjk22071998@gmail.com>
2022-03-08 18:19:51 +0500

    DBHelper ready

---
+++ lib/database/sqflite_database.dart
@@ -0,0 +1,52 @@
+import 'package:note_taking_app/model/notes.dart';
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+
+class DBHelper {
+  DBHelper._();
+  static final DBHelper dbHelper = DBHelper._();
+  late Database _database;
+
+  Future<Database> get database async {
+    if (_database != null) {
+      return _database;
+    }
+    _database = initDB();
+    return _database;
+  }
+
+  static const String tableName = "Notes";
+  static const String col1Name = "Title";
+  static const String col2Name = "Body";
+  static const String col3Name = "Creation_Date";
+
+  initDB() async {
+    return await openDatabase(join(await getDatabasesPath(), "notes.db"),
+        onCreate: ((db, version) async {
+      await db.execute('''
+      CREATE TABLE $tableName (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        $col1Name TEXT,
+        $col2Name TEXT,
+        $col3Name DATE
+      )
+      ''');
+    }), version: 1);
+  }
+
+  addNote(Notes note) async {
+    final db = await database;
+    db.insert(tableName, note.toMap(),
+        conflictAlgorithm: ConflictAlgorithm.replace);
+  }
+
+  Future<dynamic> getAllNotes() async {
+    final db = await database;
+    List<Map> res = await db.query(tableName);
+    if (res.isEmpty) {
+      return null;
+    } else {
+      return res.toList();
+    }
+  }
+}
